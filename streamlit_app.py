4562cbe2d6f9377fd63d7c751db582045666ab3f
Juan Martin <juanmar.londono@gmail.com>
2025-07-14 16:24:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,7 @@ from folium.plugins import HeatMap, HeatMapWithTime, MarkerCluster, Fullscreen
 st.markdown("""
     <style>
     /* Multiselect customization */
-    .stMultiSelect [data-baseweb="tag"] {
-        background-color: #9c3675 !important;
-    }
-    .stMultiSelect [data-baseweb="tag"] div {
-        color: white !important;
-    }
-    .stMultiSelect > div {
-        border-color: #9c3675 !important;
-    }
-    .stMultiSelect svg {
-        color: #9c3675 !important;
-    }
+
 
     /* Checkbox customization */
     input[type="checkbox"] + div svg {
